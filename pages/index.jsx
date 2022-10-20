a34ef89d14af23331f4b8d45de53b472cb36c185
ms198 <muhamadsobari198@gmail.com>
2022-10-20 17:33:59 +0700

    update pages/index.jsx - add font family

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
-import { Box } from '@material-ui/core';
+
+import Head from 'next/head';
+import theme from '../src/theme';
+
 import Feed from '../src/components/Feed';
 
 export default function Index() {
 	return (
-		<Box>
+		<React.Fragment>
+			<Head>
+				<meta charSet="utf-8" />
+				<meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+				/>
+				<meta name="theme-color" content={theme.palette.primary.main} />
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+				/>
+				<title>News App - Next.js - M.Sobari</title>
+			</Head>
 			<Feed/>
-		</Box>
+		</React.Fragment>
 	);
 }
