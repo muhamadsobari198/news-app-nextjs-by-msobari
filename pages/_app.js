a43fb925563e7db2bdf9ba748bbc455cb693cc89
ms198 <muhamadsobari198@gmail.com>
2022-10-20 18:03:55 +0700

    moving head element to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../src/theme';
 import Layout from '../src/components/Layout';
 
+
 import  { store, persistor } from '../src/redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -22,6 +23,19 @@ function MyApp({ Component, pageProps }) {
 		  	<Provider store={store}>
 				<PersistGate loading={null} persistor={persistor}>
 					<ThemeProvider theme={theme}>
+						<Head>
+							<meta charSet="utf-8" />
+							<meta
+								name="viewport"
+								content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+							/>
+							<meta name="theme-color" content={theme.palette.primary.main} />
+							<link
+								rel="stylesheet"
+								href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+							/>
+							<title>News App - Next.js - M.Sobari</title>
+						</Head>
 						<Layout>
 							<CssBaseline />
 							<Component {...pageProps} />
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,9 @@
 import * as React from 'react';
-
-import Head from 'next/head';
-import theme from '../src/theme';
-
 import Feed from '../src/components/Feed';
 
 export default function Index() {
 	return (
 		<React.Fragment>
-			<Head>
-				<meta charSet="utf-8" />
-				<meta
-					name="viewport"
-					content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-				/>
-				<meta name="theme-color" content={theme.palette.primary.main} />
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-				/>
-				<title>News App - Next.js - M.Sobari</title>
-			</Head>
 			<Feed/>
 		</React.Fragment>
 	);
