8f0e501271cdb466580eb21768137eb94252e776
ms198 <muhamadsobari198@gmail.com>
2022-10-20 16:48:05 +0700

    update _app.js - Add Provider store for manage state

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,10 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../src/theme';
 import Layout from '../src/components/Layout';
 
+import  { store, persistor } from '../src/redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 function MyApp({ Component, pageProps }) {
 	useEffect(() => {
 		const jssStyles = document.querySelector('#jss-server-side');
@@ -15,12 +19,16 @@ function MyApp({ Component, pageProps }) {
 	}, []);
 
 	return (
-		<ThemeProvider theme={theme}>
-			<Layout>
-				<CssBaseline />
-				<Component {...pageProps} />
-			</Layout>
-		</ThemeProvider>
+		  	<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					<ThemeProvider theme={theme}>
+						<Layout>
+							<CssBaseline />
+							<Component {...pageProps} />
+						</Layout>
+					</ThemeProvider>
+				</PersistGate>
+			</Provider>
 	);
 }
 
